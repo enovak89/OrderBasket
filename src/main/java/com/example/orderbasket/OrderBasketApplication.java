4399e8ebce86bf0bb27159d1c20799c4db22583e
enovak89 <enovak89@mail.ru>
2023-03-21 23:27:10 +0300

    init commit

---
+++ src/main/java/com/example/orderbasket/OrderBasketApplication.java
@@ -0,0 +1,13 @@
+package com.example.orderbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderBasketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderBasketApplication.class, args);
+    }
+
+}
